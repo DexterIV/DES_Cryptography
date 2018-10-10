d3c7ca478b86239390ddfff7613fbf4fc7f30fd1
DexterIV <wiedzmenpl@gmail.com>
2018-10-10 19:19:27 +0200

    Add files via upload

---
+++ Cryptography_DES/BitExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace Cryptography.TripleDes
+{
+    public static class BitExtensions
+    {
+        public static BitArray Append(this BitArray current, BitArray after)
+        {
+            var bools = new bool[current.Count + after.Count];
+            current.CopyTo(bools, 0);
+            after.CopyTo(bools, current.Count);
+            return new BitArray(bools);
+        }
+
+        public static BitArray[] Split(this BitArray current)
+        {
+            var half1 = new bool[current.Length / 2];
+            var half2 = new bool[current.Length / 2];
+
+            for (var i = 0; i < current.Length / 2; i++)
+            {
+                half1[i] = current[i];
+                half2[i] = current[i + current.Length / 2];
+            }
+
+            return new[] {new BitArray(half1), new BitArray(half2)};
+        }
+
+        public static string Debug(this BitArray bits)
+        {
+            var str = "";
+            for (var i = 0; i < bits.Length; i++)
+                str += bits[i].BoolToNumber();
+            System.Diagnostics.Debug.WriteLine(str);
+            return str;
+        }
+
+        public static string BoolToNumber(this bool b)
+        {
+            return b ? "1" : "0";
+        }
+    }
+}
---
+++ Cryptography_DES/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Cryptography.TripleDes
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DecryptButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var des = new DesEncryption();
+            var res = des.Decrypt(Input.Text, Key1.Text);
+
+            Output.Text = res;
+        }
+
+        private void EncryptButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var des = new DesEncryption();
+            var res = des.Encrypt(Input.Text, Key1.Text);
+
+            Output.Text = res;
+        }
+
+        private void ButtonToHexOnClick(object sender, RoutedEventArgs e)
+        {
+            Input.Text =  string.Join("",Encoding.ASCII.GetBytes(Input.Text).Select(c => c.ToString("X2")));
+        }
+
+        private void UtfButtonOnClick(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                Output.Text = Regex.Replace(new string(Encoding.ASCII.GetChars(FromHex(Output.Text))).Trim(), @"[^\u0020-\u007E]", string.Empty);
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+    }
+}
